40164d1339bd78c32bba35180310291bbb3d707c
Agustin M. Ruiz <agustinmruiz29@gmail.com>
2025-10-27 20:25:45 -0300

    new open graph

--- src/App.jsx
+++ src/App.jsx
@@ -21,12 +21,12 @@ function App() {
         <meta property="og:description" content="Desarrollador Web Fullstack especializado en React, Node.js y WordPress" />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://portfolio-psi-rouge-50.vercel.app/" />
-        <meta property="og:image" content="https://portfolio-psi-rouge-50.vercel.app/mapache-02-01.png" />
+        <meta property="og:image" content="https://portfolio-psi-rouge-50.vercel.app/mapache-og.png" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
         <meta property="og:image:type" content="image/png" />
         <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:image" content="https://portfolio-psi-rouge-50.vercel.app/mapache-02-01.png" />
+        <meta property="twitter:image" content="https://portfolio-psi-rouge-50.vercel.app/mapache-og.png" />
         {/* Favicon */}
         <link rel="icon" type="image/png" sizes="32x32" href="/mapache-02-01.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/mapache-02-01.png" />
